a89d2bb81dfed1a1fe3f284db7366f6f254d1185
Pasindu Promod <dev@ninehermits.com>
2024-07-26 12:35:11 +0530

    invite button animation

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,6 +1,33 @@
+const cacheName = "DefaultCompany-Plank Pushers-0.1";
+const contentToCache = [
+    "Build/build.loader.js",
+    "Build/820c80cc3485b7af1bb5807e5ea2f8a5.js.unityweb",
+    "Build/81c938fd058ceedf1c9f119bacae9065.data.unityweb",
+    "Build/4911001ab12e7888f04a3a458a8abf81.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
 });
 
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
