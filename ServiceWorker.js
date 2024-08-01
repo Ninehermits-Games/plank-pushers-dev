6e4aa17c8f0268dc8eb6508fe0dc671a5de82291
Pasindu Promod <dev@ninehermits.com>
2024-08-02 01:35:16 +0530

    stats ui fixed

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,7 +2,7 @@ const cacheName = "DefaultCompany-Plank Pushers-0.1";
 const contentToCache = [
     "Build/build.loader.js",
     "Build/ebe136c30b9ba03825c6547021231394.js.unityweb",
-    "Build/bc7bf6b143c487a6537f913f2162c583.data.unityweb",
+    "Build/484d392e13b4f3e8c690540c54ac81dd.data.unityweb",
     "Build/d4c896003b4b26904a9ecd88d4393e7b.wasm.unityweb",
     "TemplateData/style.css"
 
