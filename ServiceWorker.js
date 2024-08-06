95919b05379794e7d632869859c25132d6896edc
Pasindu Promod <dev@ninehermits.com>
2024-08-06 09:28:43 +0530

    cache test 3

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -11,6 +11,22 @@ const contentToCache = [
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
+
+e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => {
+            return name !== cacheName;
+          })
+          .map((name) => {
+            console.log("[Service Worker] Deleting old cache:", name);
+            return caches.delete(name);
+          })
+      );
+    })
+  );
+  
     e.waitUntil((async function () {
       const cache = await caches.open(cacheName);
       console.log('[Service Worker] Caching all: app shell and content');
