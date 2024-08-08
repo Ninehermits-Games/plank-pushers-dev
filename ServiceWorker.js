2c34e2c4d8c83a68e0fb8cc8f58d1ab29a6e14a1
Pasindu Promod <dev@ninehermits.com>
2024-08-08 14:59:42 +0530

    new game test

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
-const cacheName = "DefaultCompany-Plank Pushers-0.2";
+const cacheName = "DefaultCompany-Plank Pushers-0.9";
 const contentToCache = [
     "Build/build.loader.js",
-    "Build/30fad2ef5e2678905ddebba07fd2f999.js.unityweb",
-    "Build/3cc3ce3b2e012a8d9716cd00a3f45656.data.unityweb",
-    "Build/e9eedb033b9cfcd6107a376c2122c99e.wasm.unityweb",
+    "Build/f22fe9d745881ec8e993ae6dddbab004.js",
+    "Build/9d8494ed009d31deb41a03301bec4dd7.data",
+    "Build/fefd4981a692922fccb215fae634967a.wasm",
     "TemplateData/style.css"
 
 ];
@@ -11,22 +11,6 @@ const contentToCache = [
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
-
-e.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames
-          .filter((name) => {
-            return name !== cacheName;
-          })
-          .map((name) => {
-            console.log("[Service Worker] Deleting old cache:", name);
-            return caches.delete(name);
-          })
-      );
-    })
-  );
-  
     e.waitUntil((async function () {
       const cache = await caches.open(cacheName);
       console.log('[Service Worker] Caching all: app shell and content');
