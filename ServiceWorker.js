fcd5c24cb0bbc24513f84b8ce0d7b3d922922484
Pasindu Promod <dev@ninehermits.com>
2024-08-09 16:03:11 +0530

    start fuctions removed

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-Plank Pushers-0.9";
 const contentToCache = [
     "Build/build.loader.js",
-    "Build/cb67e0431b2cb3951506359b74d913c5.js.unityweb",
-    "Build/cfaeea8b9c463d0a8fa465ea4986a87f.data.unityweb",
-    "Build/90f62b645396ef87b653bdaef21e54ea.wasm.unityweb",
+    "Build/085f769b234d1c593ac5a52c62b80ea8.js",
+    "Build/2bbbce51d402ba0707933a3b686e058f.data",
+    "Build/5ea5596f4fbf3d5a6b26b7026ad88b7a.wasm",
     "TemplateData/style.css"
 
 ];
